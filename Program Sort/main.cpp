923659e02daf4b3e583688a42a615c77c521c3b1
20161104597 <Stereophonic@admindeiMac-34.local>
2018-07-08 20:03:21 +0800

    f
    f

--- Program Sort/main.cpp
+++ Program Sort/main.cpp
@@ -1,9 +1,15 @@
 //节目类型说明
+//sing 唱歌
+//dance 跳舞
+//drama 舞台剧
+//chant 朗诵
+//rap 说唱
 #include<iostream>
 #include<string>
+#include<stdio.h>
 using namespace std;
-int p,q,p1,q1;            //p，q分别为选手和裁判的个数 个数等于录入与读入之和（eg:p=p1+p2）
-int p2=0,q2=0;      //将文件中选手和评委个数均初始化为0；
+//p，q分别为选手和裁判的个数 个数等于录入与读入之和（eg:p=p1+p2）
+int p=0,q=0;      //将文件中选手和评委个数均初始化为0；
 struct player{
     string name;
     string sex;
@@ -16,21 +22,23 @@ struct player{
     double min;
     double avg;
     double sum;
-    };
+};
 struct judge{
     string num;
     string name;
     string sex;
     
 };
-void player()
+
+struct player s1[100];
+struct judge s2[100];
+void play(player *s1)
 {
-    struct player s1[100];
     int i;       //p1为程序中录入的选手的个数
     cout<<"请输入要录入的选手个数:"<<endl;
-    cin>>p1;
+    cin>>p;
     cout<<"请依次录入选手姓名、性别、节目名称、节目类型、选手班级与选手电话"<<endl;
-    for(i=0;i<p1;i++)
+    for(i=1;i<=p;i++)
     {
         cin>>s1[i].name;
         cin>>s1[i].sex;
@@ -40,37 +48,69 @@ void player()
         cin>>s1[i].tel;
     }
     cout<<"已成功录入选手信息!"<<endl;
-
-
 }
-void judge()
-    {
-        struct judge s2[20];
-        int i;           //q1为程序中录入的评委的个数
-        cout<<"请输入要录入的评委个数:"<<endl;
-        cin>>q1;
-        cout<<"请录入评委编号、姓名与性别"<<endl;
-        for(i=0;i<q1;i++)
-        {
-            cin>>s2[i].num;
-            cin>>s2[i].name;
-            cin>>s2[i].sex;
-
-        }
-        cout<<"已成功录入评委信息!"<<endl;
-    }
-
-void begin()
+void jud(judge *s2)
 {
-    struct player s1[100];
+    int i;           //q1为程序中录入的评委的个数
+    cout<<"请输入要录入的评委个数:(至少三个评委)"<<endl;
+    cin>>q;
+    cout<<"请录入评委编号、姓名与性别"<<endl;
+    for(i=0;i<q;i++)
+    {
+        cin>>s2[i].num;
+        cin>>s2[i].name;
+        cin>>s2[i].sex;
+        
+    }
+    cout<<"已成功录入评委信息!"<<endl;
+}
+
+
+/*void Readplayer()
+ 321{
+ FILE* fp;
+ int i,k;
+ fp=fopen("//Users//Stereophonic//Desktop//Program\ Sort//judge.csv","w");
+ if(fp == NULL)
+ cout<<"文件打开失败"<<endl;
+ else
+ {
+ k=1;
+ fprintf(fp,"选手姓名、选手性别、节目名称、节目类型、选手班级与选手电话\n");
+ for(i=0;i<p2;i++)
+ {
+ fprintf(fp,"%s,%s,%s,%s,%s,%s\n",)
+ k++
+ }
+ }
+ 
+ }
+ void Readjudge()
+ {
+ FILE* fp;
+ int i,k;
+ fp=fopen("//Users//Stereophonic//Desktop//Program\ Sort//judge.csv","w");
+ if(fp == NULL)
+ cout<<"文件打开失败"<<endl;
+ else
+ {
+ k=1;
+ fprintf(fp,"评委编号，评委姓名，评委性别,\n");
+ for(i=0;i<q2;i++)
+ {
+ fprintf(fp,"%s,%s,%s\n",)
+ k++
+ }
+ }
+ 
+ 
+ }*/
+void begin(player *s1)
+{
     int n,i,z;
-    p=p1+p2;
-    q=q1+q2;
-    
     cout<<"开始打分"<<endl;
     for(n=1;n<=p;n++)
     {
-        cout<<1<<endl;
         cout<<"请给第"<<n<<"位选手打分："<<endl;
         for(i=1;i<=q;i++)
         {
@@ -103,96 +143,205 @@ void begin()
         }
         s1[n].avg=(s1[n].sum-s1[n].min-s1[n].max)/(q-2);
         cout<<"平均分为："<<s1[n].avg<<endl;
+        cout<<"总分为："<<s1[n].sum<<endl;
     }
-    
-    
-    
-
-    
-    
-    
-    
-
 }
-
-
-
-
-
-
-/*void Readplayer()
+void save(player *s1)
 {
-    FILE* fp;
-    int i,k;
-    fp=fopen("//Users//Stereophonic//Desktop//Program\ Sort//judge.csv","w");
-    if(fp == NULL)
-        cout<<"文件打开失败"<<endl;
+    FILE *fp;
+    int i,n;
+    fp=fopen("C:\\Users\\Administrator\\Desktop\\save.txt","w");
+    if(fp==NULL)
+    {
+        cout<<"文件保存失败！\n"<<endl;
+    }
     else
     {
-        k=1;
-        fprintf(fp,"评委编号，评委姓名，评委性别,\n");
-        for(i=0;i<q2;i++)
+        fprintf(fp,"选手姓名、性别、节目名称、节目类型、选手班级与选手电话、总分、最高分、最低分、平均分\n");
+        for(i=1;i<=p;i++)
         {
-            fprintf(fp,"%s,%s,%s\n",)
-            k++
+            fprintf(fp,"%s,%s,%s,%s,%s,%s,%lf,%lf,%lf,%lf\n",
+                    s1[i].name.c_str(),s1[i].sex.c_str(),s1[i].program.c_str(),s1[i].type.c_str(),s1[i].classs.c_str(),s1[i].tel.c_str(),s1[i].sum,s1[i].max,s1[i].min,s1[i].avg);
+            
+            
+        }
+        cout<<("信息保存成功！\n")<<endl;
+        fclose(fp);
+    }
+}
+void displayPlayer(player *s1)
+{
+    int i;
+    if(p==0)
+    {
+        cout<<"未录入选手信息！"<<endl;
+    }
+    else
+    {
+        cout<<"已录入选手信息如下:"<<endl;
+        for(i=1;i<=p;i++)
+        {
+            cout<<"选手姓名："<<s1[i].name<<endl;
+            cout<<"选手性别："<<s1[i].sex<<endl;
+            cout<<"节目类型："<<s1[i].program<<endl;
+            cout<<"节目类型："<<s1[i].type<<endl;
+            cout<<"选手班级："<<s1[i].classs<<endl;
+            cout<<"选手电话："<<s1[i].tel<<endl;
+            cout<<"-----------------"<<endl;
         }
     }
-
+    
+    
     
 }
-
-
-void Readjudge()
+void displayJudge(judge *s2)
 {
+    int i;
+    if(q==0)
+    {
+        cout<<"未录入评委信息！"<<endl;
+    }
+    else
+    {
+        cout<<"已录入评委信息如下:"<<endl;
+        for(i=0;i<q;i++)
+        {
+            cout<<"评委编号："<<s2[i].num<<endl;
+            cout<<"评委姓名："<<s2[i].name<<endl;
+            cout<<"评委性别："<<s2[i].sex<<endl;
+            cout<<"-----------------"<<endl;
+        }
+    }
     
     
     
-}*/
+}
+void sort(player *s1)
+{
+    struct player temp;
+    int i,j;
+    for(i=0;i<p;i++)
+    {
+        for(j=0;j<p-i;j++)
+        {
+            if(s1[j].avg<s1[j+1].avg)
+            {
+                temp=s1[j];
+                s1[j]=s1[j+1];
+                s1[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<p;i++)
+    {
+        cout<<"选手姓名:"<<s1[i].name;
+        cout<<"    节目名称:"<<s1[i].program;
+        cout<<"    节目类型"<<s1[i].type;
+        cout<<"    平均分"<<s1[i].avg<<endl;
+        cout<<"-----------------"<<endl;
+        
+    }
+}
+/*void typesort(player *s1)
+ {
+	string n;
+	struct player a[15];
+	struct player b;
+	int i=0,count=0;
+	int j;
+	cout<<"请输入需要排序的节目类型:"<<endl;
+	cin>>n;
+	for(j=0;j<p;j++)
+	{
+ if(n==s1[i].type)
+ {
+ a[count]=s1[i].type;
+ count++;
+ i++;
+ }
+ else
+ {
+ i++;
+ }
+ 
+	}
+	for(i=0;i<count;i++)
+	{
+ for(j=0;j<count-i;j++)
+ {
+ if(a[j].avg<a[j+1].avg)
+ {
+ b=a[j];
+ a[j]=a[j+1];
+ a[j+1]=b;
+ }
+ }
+	}
+	for(i=0;i<count;i++)
+	{
+ cout<<"选手姓名："<<s1[i].name;
+ cout<<"节目名称:"<<s1[i].program;
+ cout<<"节目类型"<<s1[i].type;
+ cout<<"平均分"<<s1[i].avg<<endl;
+	}
+ }
+ */
 
 int main()
 {
     int x;
     while(1)
     {
-        cout<<"----欢迎进入打分系统----"<<endl;
-        cout<<"----1.录入选手信息----"<<endl;
-        cout<<"----2.录入评委信息----"<<endl;
-        cout<<"----3.读入选手信息----"<<endl;
-        cout<<"----4.读入评委信息----"<<endl;
-        cout<<"----5.开始打分----"<<endl;
-        cout<<"----6.保存分数信息----"<<endl;
-        cout<<"----7.退出打分系统----"<<endl;
+        cout<<"—————————————"<<endl;
+        cout<<"|----欢迎进入打分系统---|"<<endl;
+        cout<<"|----1.录入选手信息-----|"<<endl;
+        cout<<"|----2.录入评委信息-----|"<<endl;
+        cout<<"|----3.显示选手信息-----|"<<endl;
+        cout<<"|----4.显示评委信息-----|"<<endl;
+        cout<<"|----5.开始打分---------|"<<endl;
+        cout<<"|----6.选手平均分排序---|"<<endl;
+        cout<<"|----7.类型平均分排序---|"<<endl;
+        cout<<"|----8.保存分数信息-----|"<<endl;
+        cout<<"|----9.退出打分系统-----|"<<endl;
+        cout<<"—————————————"<<endl;
+        cout<<"|Copyright  2018 Vincent. All Rights Reserved.|"<<endl;
         
         do
         {
-            cout<<"请输入'1-7'选项:"<<endl;
+            cout<<"请输入'1-9'选项:"<<endl;
             cin>>x;
         }
-        while(x<1 ||x>7);
+        while(x<1 ||x>9);
         
         switch(x)
         {
             case 1:
-                player();
+                play(s1);
                 break;
             case 2:
-                judge();
+                jud(s2);
                 break;
             case 3:
-                //read Readplayer();
+                displayPlayer(s1);
                 break;
             case 4:
-                //Readjudge();
+                displayJudge(s2);
                 break;
-
             case 5:
-                begin();
+                begin(s1);
                 break;
-            case 7:
+            case 6:
+                sort(s1);
+                break;
+                /*case 7:
+                 typesort(s1);*/
+            case 8:
+                save(s1);
+                break;
+            case 9:
                 cout<<"thanks for using!"<<endl;
                 exit(1);
         }
     }
-    
     return 0;
 }
